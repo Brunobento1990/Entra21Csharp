7d448adc109d546e55e31609012f54e5475b19a4
guest01 <guest01@ITELABD03>
2022-04-20 21:42:42 -0300

    Ultimo exercício

---
+++ PrimeiraAplicação/CalcularDistancia/CalcularDistancia/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CalcularDistancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcucalar distancia
+
+            double Lat1, Alt1, Lat2, Alt2, r1,r2,tot1,tot2,r11,r22;
+
+            Console.WriteLine("Digite a Latitude");
+            Alt1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a Longitude");
+            Lat1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a segunda Latitude");
+            Lat2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a segunda Longitude");
+            Alt2 = Convert.ToDouble(Console.ReadLine());
+            r1 = Alt1 - Alt2;
+            r2 = Lat1 - Lat2;
+            r11 = r1 * r1;
+            r22 = r2 * r2;
+
+            tot1 = r11 + r22;
+            tot2 = tot1 * tot1;
+
+            Console.WriteLine("A distância é " + tot2 + " Km");
+
+        }
+    }
+}
