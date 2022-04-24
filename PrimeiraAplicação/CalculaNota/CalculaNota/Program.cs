5a5fc181437ab25e10e8793ec32d8479e97a4120
Bruno Bento gonçalves <brunobentocaina@gmail.com>
2022-04-24 18:31:50 -0300

    Ajuste calculo nota media

--- PrimeiraAplicação/CalculaNota/CalculaNota/Program.cs
+++ PrimeiraAplicação/CalculaNota/CalculaNota/Program.cs
@@ -17,18 +17,20 @@ namespace CalculaNota
 
             double media = (not1 + not2 + not3) / 3;
 
-            if (not1 > not2 && not1 > not3 && not3 < not2)
+            if (not1 >= not2 && not1 > not3 && not3 < not2)
             {
                 Console.WriteLine("Sua maior nota é " + not1 + " sua menor nota é " + not3 + " e sua média é " + media);
             }
-            if (not2 > not1 && not2 > not3 && not1 < not3)
+            if (not2 >= not3 && not2 > not1 && not1 < not3)
             {
                 Console.WriteLine("Sua maior nota é " + not2 + " sua menor nota é " + not1 + " e sua média é " + media);
             }
-            if (not3 > not1 && not3 > not2 && not2 < not1)
+            if (not3 >= not1 && not3 > not2 && not2 < not1)
             {
                 Console.WriteLine("Sua maior nota é " + not3 + " sua menor nota é " + not2 + " e sua média é " + media);
             }
+            
+
 
         }
     }
