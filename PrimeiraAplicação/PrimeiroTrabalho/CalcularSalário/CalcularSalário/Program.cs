01fd7e58bb7be33f5360254fa451feb6b324fcc6
guest01 <guest01@ITELABD03>
2022-04-27 19:07:44 -0300

    Arquivo deletado

---
+++ PrimeiraAplicação/PrimeiroTrabalho/CalcularSalário/CalcularSalário/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CalcularSalário
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salar1, aliquota, salarTot, salarDolar, salarEuro;
+            Console.WriteLine("Qual seu salário?");
+            salar1 = Convert.ToDouble(Console.ReadLine());
+
+
+            if(salar1 <= 1200)
+            {
+                aliquota = (salar1 * 20) / 100;
+                salarTot = salar1 + aliquota;
+                salarDolar = salarTot / 4.70;
+                salarEuro = salarTot / 5.20;
+                Console.WriteLine("Seu aumento é de 20%, seu salário total é " + salarTot + " em dólar " + salarDolar + " em Euro " + salarEuro);
+            }
+            if(salar1 > 1200 && salar1 <= 2000)
+            {
+                aliquota = (salar1 * 13) / 100;
+                salarTot = salar1 + aliquota;
+                salarDolar = salarTot / 4.70;
+                salarEuro = salarTot / 5.20;
+                Console.WriteLine("Seu aumento é de 13%, seu salário total é " + salarTot + " em dólar " + salarDolar + " em Euro " + salarEuro);
+            }
+            if(salar1 > 2000 && salar1 <= 2500)
+            {
+                aliquota = (salar1 * 10) / 100;
+                salarTot = salar1 + aliquota;
+                salarDolar = salarTot / 4.70;
+                salarEuro = salarTot / 5.20;
+                Console.WriteLine("Seu aumento é de 10% ,seu salário total é " + salarTot + " em Dólar " + salarDolar + " em Euro " + salarEuro);
+            }
+            if(salar1 > 2500)
+            {
+                aliquota = (salar1 * 5) / 100;
+                salarTot = salar1 + aliquota;
+                salarDolar = salarTot / 4.70;
+                salarEuro = salarTot / 5.20;
+                Console.WriteLine("Seu aumento é de 5%, seu salário total é " + salarTot + " em Dólar " + salarDolar + " em Euro " + salarEuro);
+            }
+        }
+    }
+}
---
+++ PrimeiraAplicação/PrimeiroTrabalho/NumeroCrescente/NumeroCrescente/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NumeroCrescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3;
+            Console.WriteLine("Insira o primerio número");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insira o segundo número");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insira o terceiro número");
+            n3 = Convert.ToDouble(Console.ReadLine());
+            if(n1 > n2 && n1 > n3 && n2 > n3)
+            {
+                Console.WriteLine("Seu números em ordem crescente são " + n3 + " " + n2 + " " +n1);
+            }
+            if(n1 > n2 && n1 > n3 && n3 > n2)
+            {
+                Console.WriteLine("Seus números em ordem crescente são " + n2 + " " + n3 + " " + n1);
+            }
+            if(n2 > n1 && n2 > n3 && n1 > n3)
+            {
+                Console.WriteLine("Seus números em ordem crescente são " + n3 + " " + n1 + " " + n2);
+            }
+            if(n2 > n1 && n2 > n3 && n3 > n1)
+            {
+                Console.WriteLine("Seus números em ordem crescente são " + n1 + " " + n3 + " " + n2);
+            }
+            if(n3 > n1 && n3 > n2 && n1 > n2)
+            {
+                Console.WriteLine("Seus números em ordem crescente são " + n2 + " " + n1 + " " + n3);
+            }
+            if(n3 > n1 && n3 > n2 && n2 > n1)
+            {
+                Console.WriteLine("Seus números em ordem crescente são " + n1 + " " + n2 + " " + n3);
+            }
+            if(n1 == n2 && n1 > n3)
+            {
+                Console.WriteLine("Há 2 números iguais e a ordem dos números é " + n3 + " " + n1);
+            }
+            if(n1 == n3 && n1 > n2)
+            {
+                Console.WriteLine("Há 2 números iguais e a ordem dos números é " + n2 + " " + n1);
+            }
+            if(n2 == n3 && n2 > n1)
+            {
+                Console.WriteLine("Há 2 números iguas e a ordem dos números e " + n1 + " " + n2);
+            }
+        }
+    }
+}
