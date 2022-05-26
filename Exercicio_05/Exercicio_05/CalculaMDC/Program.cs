2c9ea188ad609f18697d4679fe8a228e01c3bbcf
Bruno <brunobentocaina@gmail.com>
2022-05-26 19:37:43 -0300

    Finalizado exercicio 5

--- Exercicio_05/Exercicio_05/CalculaMDC/Program.cs
+++ Exercicio_05/Exercicio_05/CalculaMDC/Program.cs
@@ -6,7 +6,7 @@ namespace CalcularMMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("---> Calcular Mínimo Multiplo Comum de 3 números inteiros <---");
+            Console.WriteLine("---> Calcular Máximo Divisor Comum de 3 números inteiros <---");
             Console.WriteLine();
             Console.WriteLine("---> Insira o 1° número !");
             int n1 = Convert.ToInt32(Console.ReadLine());
@@ -15,36 +15,36 @@ namespace CalcularMMC
             Console.WriteLine("---> Insira o 3° número !");
             int n3 = Convert.ToInt32(Console.ReadLine());
             int Result = CalcularMMC(n1, n2, n3);
-            Console.WriteLine($"---> O Mínimo Multiplo Comum de {n1} , {n2} , {n3} é {Result}");
+            Console.WriteLine($"---> O Máximo Divisor Comum de {n1} , {n2} , {n3} é {Result}");
 
         }
-        static int CalcularMMC ( int n1,int n2,int n3)
+        static int CalcularMMC(int n1, int n2, int n3)
         {
 
             int Result = 1, Divisor = 2;
-            while(n1 > 1 || n2 > 1 || n3 > 1)
+            while (n1 > 1 || n2 > 1 || n3 > 1)
             {
 
-                if(n1%Divisor==0 || n2 % Divisor == 0 || n3 % Divisor == 0)
+                if (n1 % Divisor == 0 && n2 % Divisor == 0 && n3 % Divisor == 0)
                 {
-                    
-                    if (n1 % Divisor == 0)
-                    {
-                        n1 /= Divisor;
-                        
-                    }
-                    if (n2 % Divisor == 0)
-                    {
-                        n2 /= Divisor;
-
-                    }
-                    if (n3 % Divisor == 0)
-                    {
-                        n3 /= Divisor;
-                    }
                     Result *= Divisor;
+                   
                 }
-              
+                if (n1 % Divisor == 0)
+                {
+                    n1 /= Divisor;
+                    
+                }
+                if (n2 % Divisor == 0)
+                {
+                    n2 /= Divisor;
+
+                }
+                if (n3 % Divisor == 0)
+                {
+                    n3 /= Divisor;
+                }
+                
                 else
                 {
                     Divisor++;
